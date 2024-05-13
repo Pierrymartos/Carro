09a29d827678f3954888e2080962603b27604ca6
Pierrymartos <105867296+Pierrymartos@users.noreply.github.com>
2024-05-13 14:12:17 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,15 @@
+let Fundo;
+let imagemCarro;
+let imagemCarro1;
+let imagemCarro2;
+let imagemAtor;
+
+
+ function preload(){
+  Fundo = loadImage("imagens/estrada.png");
+  imagemCarro = loadImage("imagens/carro5.png");
+   imagemCarro1 = loadImage("imagens/carro4.png");
+   imagemCarro2 = loadImage("imagens/carro.png");
+   imagemAtor = loadImage("imagens/ator-1.png");
+  imagemCarros = [imagemCarro, imagemCarro1, imagemCarro2,imagemCarro, imagemCarro1, imagemCarro2];   
+  }
